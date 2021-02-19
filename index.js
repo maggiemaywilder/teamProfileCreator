41bead36227247849625229cf46e26ea4af3fd4c
Maggie may <maggiemaywilder@gmail.com>
2021-02-19 12:59:33 -0600

    finalize prompts and generate team array

--- index.js
+++ index.js
@@ -1,59 +1,92 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/employee.js');
+const Employee = require('./lib/employee');
+// const generatePage = require('./dist/generatePage.js');
 
+const team = [];
+var member;
 
   // function of questions to ask about team members
     // need last question to change with type of member
   // function to add member and push to array using the prompt from above
 
-function newMember(type) {
-  const person = type;
-  const lastQ = {
-    'team manager': `What is the team manager's office number?`,
-    'engineer': `What is your engineer's GitHub username?`,
-    'intern': `What is your intern's school?`
-  };
-  const correctQ = lastQ[person];
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: `name`,
-      message: `What is the ${person}'s name?`
-    },      
-    {
-      type: 'number',
-      name: `id`,
-      message: `What is the ${person}'s id?`
-    },
-    {
-      type: 'input',
-      name: `email`,
-      message: `What is the ${person}'s email?`
-    },
-    {
-      type: 'input',
-      name: 'last',
-      message: `${correctQ}`
-    },
-  ])
-  .then ((answers) => {
-    console.log(person, answers); //will change to creating object and pushing
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      console.log(`Prompts couldn't be rendered in current environment.`);
-    } else {
-      console.log('error');
-    }
-  })
-
+  function newMember(type) {
+    const person = type;
+    member = [];
+    member.push(type);
+    const lastQ = [
+      {key: 'team manager',
+       value: `What is the team manager's office number?`},
+      {key: 'engineer',
+       value: `What is your engineer's GitHub username?`},
+      {key: 'intern',
+       value: `What is your intern's school?`}
+    ];
+    const finalQ = lastQ.find(({key}) => key === type);
+    console.log(finalQ.value);
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: `name`,
+        message: `What is the ${type}'s name?`
+      },      
+      {
+        type: 'number',
+        name: `id`,
+        message: `What is the ${person}'s id?`
+      },
+      {
+        type: 'input',
+        name: `email`,
+        message: `What is the ${person}'s email?`
+      },
+      {
+        type: 'input',
+        name: `last`,
+        message: finalQ.value
+      },
+    ])
+    .then ((answers) => {
+      member.push(answers);
+      console.log(member);
+      team.push(member);
+      next();
+    })
+    .catch((error) => {
+      if(error.isTtyError) {
+        console.log(`Prompts couldn't be rendered in current environment.`);
+      } else {
+        console.log('error');
+      }
+    });
   }
-
-
-// variable type, use type to complete questions, use when to pick last question, use answers to create new Employee, push to array of team members, ask next member, return type. In init, if !type array complete, else run new member again passing in type.
+  
+  function next() {
+    inquirer.prompt([
+      {
+        type: 'expand',
+        name: 'memberType',
+        message: `Which type of team member would you like to add?`,
+        choices: [
+          {key: 'm', name: 'Manager', value: 'team manager'},
+          {key: 'e', name: 'Engineer', value: 'engineer'},
+          {key: 'i', name: 'Intern', value: 'intern'},
+          {key: 'n', name: `I would not like to add any further team members.`, value: `none`}
+        ],
+      },
+    ])
+    .then ((answer) => {
+      newP = answer.memberType;
+      if(newP === 'none') {
+        console.log(team)
+      } else {
+        newMember(newP);
+      }
+    });
+  }
 
 function init() {
   newMember('team manager');
+
 }
 
 
