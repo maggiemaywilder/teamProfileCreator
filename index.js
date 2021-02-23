8cddcc664f5cec1123212b30294117909bdff843
Maggie may <maggiemaywilder@gmail.com>
2021-02-22 22:59:28 -0600

    add some functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const Employee = require('./lib/employee');
 const createPage = require('./src/generatePage.js');
+const fs = require('fs');
 
 const team = [];
 var member;
@@ -9,78 +10,86 @@ var member;
     // need last question to change with type of member
   // function to add member and push to array using the prompt from above
 
-  function newMember(type) {
-    const person = type;
-    member = [];
-    member.push(type);
-    const lastQ = [
-      {key: 'team manager',
-       value: `What is the team manager's office number?`},
-      {key: 'engineer',
-       value: `What is your engineer's GitHub username?`},
-      {key: 'intern',
-       value: `What is your intern's school?`}
-    ];
-    const finalQ = lastQ.find(({key}) => key === type);
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: `name`,
-        message: `What is the ${type}'s name?`
-      },      
-      {
-        type: 'number',
-        name: `id`,
-        message: `What is the ${person}'s id?`
-      },
-      {
-        type: 'input',
-        name: `email`,
-        message: `What is the ${person}'s email?`
-      },
-      {
-        type: 'input',
-        name: `last`,
-        message: finalQ.value
-      },
-    ])
-    .then ((answers) => {
-      member.push(answers);
-      team.push(member);
-      next();
-    })
-    .catch((error) => {
-      if(error.isTtyError) {
-        console.log(`Prompts couldn't be rendered in current environment.`);
-      } else {
-        console.log('error');
-      }
-    });
-  }
-  
-  function next() {
-    inquirer.prompt([
-      {
-        type: 'expand',
-        name: 'memberType',
-        message: `Which type of team member would you like to add?`,
-        choices: [
-          {key: 'm', name: 'Manager', value: 'team manager'},
-          {key: 'e', name: 'Engineer', value: 'engineer'},
-          {key: 'i', name: 'Intern', value: 'intern'},
-          {key: 'n', name: `I would not like to add any further team members.`, value: `none`}
-        ],
-      },
-    ])
-    .then ((answer) => {
-      newP = answer.memberType;
-      if(newP === 'none') {
-        createPage(team);
-      } else {
-        newMember(newP);
-      }
-    });
-  }
+function writeToFile(data) {
+  let fileName = (`./dist/index.html`);
+  let htmlContent = createPage(data);
+  fs.writeFile(fileName, htmlContent, (err) => 
+  err ? console.log(err) : console.log('Success!')
+  );
+}
+
+function newMember(type) {
+  const person = type;
+  member = [];
+  member.push(type);
+  const lastQ = [
+    {key: 'team manager',
+      value: `What is the team manager's office number?`},
+    {key: 'engineer',
+      value: `What is your engineer's GitHub username?`},
+    {key: 'intern',
+      value: `What is your intern's school?`}
+  ];
+  const finalQ = lastQ.find(({key}) => key === type);
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: `name`,
+      message: `What is the ${type}'s name?`
+    },      
+    {
+      type: 'number',
+      name: `id`,
+      message: `What is the ${person}'s id?`
+    },
+    {
+      type: 'input',
+      name: `email`,
+      message: `What is the ${person}'s email?`
+    },
+    {
+      type: 'input',
+      name: `last`,
+      message: finalQ.value
+    },
+  ])
+  .then ((answers) => {
+    member.push(answers);
+    team.push(member);
+    next();
+  })
+  .catch((error) => {
+    if(error.isTtyError) {
+      console.log(`Prompts couldn't be rendered in current environment.`);
+    } else {
+      console.log('error');
+    }
+  });
+}
+
+function next() {
+  inquirer.prompt([
+    {
+      type: 'expand',
+      name: 'memberType',
+      message: `Which type of team member would you like to add?`,
+      choices: [
+        {key: 'm', name: 'Manager', value: 'team manager'},
+        {key: 'e', name: 'Engineer', value: 'engineer'},
+        {key: 'i', name: 'Intern', value: 'intern'},
+        {key: 'n', name: `I would not like to add any further team members.`, value: `none`}
+      ],
+    },
+  ])
+  .then ((answer) => {
+    newP = answer.memberType;
+    if(newP === 'none') {
+      writeToFile(team);
+    } else {
+      newMember(newP);
+    }
+  });
+}
 
 function init() {
   newMember('team manager');
--- src/generatePage.js
+++ src/generatePage.js
@@ -31,16 +31,102 @@ function constructors(team) {
         internsObjs.push(student);
     });
     teamObjs = [...managersObjs, ...engineersObjs, ...internsObjs];
-    return (teamObjs);
-
-
+    makeCards(teamObjs);
     }
     
+function makeCards(objArray) {
+    objArray.forEach(function(employee){
+        switch(employee.getRole()) {
+            case Manager:
+                const managerCard = (employee) => 
+`<div class="card">
+    <div class="card-heading bg-success">
+        <h3>${employee.name}</h3>
+        <h4>${employee.getRole()}</h4>
+    </div>
+    <div class="card-body">
+        <Ul>
+            <li>ID:${employee.id}</li>
+            <li>Email: <a href=mailto:${employee.email}>${employee.email}</a>
+            <li>Office number: ${employee.officeNumber}</li>
+        </ul>
+    </div>
+</div>`;
+                break;
+            case Engineer:
+                const engineerCard = (employee) => 
+`<div class="card">
+    <div class="card-heading bg-primary">
+        <h3>${employee.name}</h3>
+        <h4>${employee.getRole()}</h4>
+    </div>
+    <div class="card-body">
+        <Ul>
+            <li>ID:${employee.id}</li>
+            <li>Email: <a href="mailto:${employee.email}">${employee.email}</a>
+            <li>GitHub: <a href="https://github.com/${employee.github}">${employee.github}</a></li>
+        </ul>
+    </div>
+</div>`;
+                break;
+            case Intern:
+                const internCard = (employee) => 
+`<div class="card">
+    <div class="card-heading bg-warning">
+        <h3>${employee.name}</h3>
+        <h4>${employee.getRole()}</h4>
+    </div>
+    <div class="card-body">
+        <Ul>
+            <li>ID:${employee.id}</li>
+            <li>Email: <a href=mailto:${employee.email}>${employee.email}</a>
+            <li>School: ${employee.school}</li>
+        </ul>
+    </div>
+</div>`;
+                break;
+        }
+    });
+};
+
+    // console.log(objArray, 'line 39');
+    // objArray.forEach(function(element) {
+    //     const card = ``
 
 function createPage(team) {
-    constructors(team);
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <style>
+        .header {
+            height: 100px;
+        }
+
+
+    </style>
+    <title>Team Profile Page</title>
+</head>
+<body>
+    <head class="container-fluid bg-info header">
+        <h1>My Team</h1>
+    </head>
+    <main class="container-lg bg-light">
+        <div class="row align-items-center">
+${constructors(team)}
+        </div>
+    </main>
+
+    <!-- Bootstrap Icons? -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>`
     // teamObjs;
-    console.log(teamObjs, 'line 43');
 
 }
 
